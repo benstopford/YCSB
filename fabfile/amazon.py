45323297ae9f0fb391ef7182e74412d91368f1ef
Ben Stopford <benstopford@gmail.com>
2014-11-21 12:07:42 +0000

    small refactor

--- fabfile/amazon.py
+++ fabfile/amazon.py
@@ -1,11 +1,10 @@
 import subprocess
-import time
-from fabric.api import env, hosts, roles, run, settings, execute, sudo, local
+from fabric.api import *
+from amazon_helper import *
 from fabfile.helpers import get_db
+from conf.hosts import refresh_roledefs
 from amazon_ip import *
-from conf.hosts import ycsb_ec2_user, refresh_roledefs
 
-dir = "fabfile/amazonctl/"
 
 def start_db_and_management(database):
     """Start both database and management nodes"""
@@ -119,54 +118,6 @@ def configure_db_instances(database):
         for command in commands:
             sudo(command)
 
-
-def wait_for_tagged_hosts_to_start(tag, count):
-    while len(get_external_ips(tag).split()) < int(count):
-        print "Waiting for %s hosts to start. Currently %s" % (count, len(get_external_ips(tag).split()))
-        time.sleep(1)
-
-    print '%s %s hosts started' % (count, tag)
-
-    # Test the ssh connection with retries
-    execute(
-        test_connection_with_wait,
-        hosts=get_external_ips(tag).split()
-    )
-
-
-def test_connection_with_wait():
-    with settings(connection_attempts=100, timeout=5):
-        run("echo 'testing connection'")
-
-
-def reboot_instances(tag):
-    ids = get_instance_ids_for_tag(tag).split()
-    local("aws ec2 reboot-instances --instance-ids %s" % " ".join(ids))
-
-
-def set_db_ssh_user(database):
-    # User must be set at database level so override global
-    global env
-    if 'ec2user' in database:
-        env.user = database['ec2user']
-
-
-def set_ycsb_ssh_user():
-    # set user back to ycsb user
-    env.user = ycsb_ec2_user
-
-
-def amazon_terminate():
-    out = Popen(["%sec2terminate" % dir], stdout=PIPE).communicate()[0]
-    print out
-
-
-def amazon_status():
-    out = Popen(["%sec2status" % dir], stdout=PIPE).communicate()[0]
-    print out
-
-
-
 def amazon_start(db):
     database = get_db(db)
 
@@ -176,13 +127,11 @@ def amazon_start(db):
     set_ycsb_ssh_user()
     start_ycsb_instances()
 
-
-
 def test():
     print env.roledefs
     print 'that was the roledef'
     db = get_db('cassandra')
-    db['start_db_function']()
+    wait_for_tagged_hosts_to_start('DB', 2)
 
 
 

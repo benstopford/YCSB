533ae13d6866dd0489cd03de19f3363068f8b4be
Ben Stopford <benstopford@gmail.com>
2015-02-04 19:31:27 +0000

    changed readme

--- fabfile/conf/workloads.py
+++ fabfile/conf/workloads.py
@@ -1,56 +1,33 @@
 root = '/opt/ycsb'  # root of YCSB installation
 # TIME_DURATION = 20*60  #seconds
 
-count = 1000
+
 conf = {
-    # global YSCB properties
-    'operationcount': count,  # Total operations (writes or reads during a workload)
-    'insertcount': count,  #How many objects to write during load phase
-    'recordcount': count,  #Specifies the keyspaace for reads
-    # 'cardinality': count / 10,
-
-    # 'maxexecutiontime': 90,
-
-    'fieldcount': 30,  # the value is a list of this many byte arrays
-    'fieldlength': 100,  # either exactly or approximately this length depending on fieldlengthdistribution
+    ## Key YSCB properties: ##
+    'operationcount': 1000,                 # Total operations (writes or reads during a workload)
+    'insertcount': 1000,                    # How many objects to write during load phase
+    'recordcount': 1000,                    # Specifies the keyspaace for reads
+        'fieldcount': 30,                   #the value is a list of this many byte arrays
+    'fieldlength': 100,                     #either exactly or approximately this length depending on fieldlengthdistribution
     'fieldlengthdistribution': 'constant',  # can also be zipfian or uniform but zipfian isn't working currently
+    # 'maxexecutiontime': 90,               #Stops the run after a set amount of time
 
+
+    #Only used for the query extension:
     # 'valuegenerator': 'queryable',
     # 'queryfield': 'field0',
-
-
-
-
-
-    # 'bulkloadsize':1000,      #loads will batch by this amount
+    # 'cardinality': 1000 / 10, #set as a fraction of the insertcount
 
     'threadcount': 5,
     'workload': 'com.yahoo.ycsb.workloads.CoreWorkload',
     'exportmeasurementsinterval': 30000,
-
-
     'insertretrycount': 10,
     'ignoreinserterrors': 'true',
     'readretrycount': 1000,
     'updateretrycount': 1000,
     'measurementtype': 'timeseries',
-    'timeseries.granularity': 100,  # Interval for reporting in ms
-
+    'timeseries.granularity': 100,
     'reconnectiontime': 1000,
-
-
-
-    #'fieldnameprefix': 'f',
-
-    #'warmupexecutiontime': 60000,
-    #'reconnectiontime': 5000, # 5 sec limit before reconnection
-    # 'reconnectionthroughput': 10, #limit for reconnection.
-    #'retrydelay': 1,
-    #'readallfields': 'false',
-    #'writeallfields': 'false',
-    #'maxexecutiontime': 600,
-    #'mongodb.writeConcern': 'replicas_safe',#Mongo SYNC only!
-
 }
 
 workloads = {
